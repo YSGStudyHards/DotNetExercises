72a617b4fdc6555a9f9a5c62b859df1c4580d8c4
Edwin <1070342164@qq.com>
2024-05-28 00:39:46 +0800

    Add FileCompressionHelper

---
+++ FileCompDecompExercise/FileCompressionHelper.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileCompDecompExercise
+{
+    /// <summary>
+    /// 文件压缩帮助类（.zip文件和.gz文件）
+    /// </summary>
+    public class FileCompressionHelper
+    {
+        public static void CreateZipFile(string sourceFolder, string zipFile)
+        {
+            if (!Directory.Exists(sourceFolder))
+            {
+                //如果文件夹不存在，则创建该文件夹
+                Directory.CreateDirectory(sourceFolder);
+            }
+
+            //创建一个新的 .zip 文件并将文件夹内容压缩进去
+            ZipFile.CreateFromDirectory(sourceFolder, zipFile);
+            Console.WriteLine("文件压缩完成");
+        }
+
+        public static void ExtractZipFile(string zipFile, string extractFolder)
+        {
+            if (!File.Exists(zipFile))
+            {
+                Console.WriteLine(".zip 文件不存在");
+                return;
+            }
+
+            if (!Directory.Exists(extractFolder))
+            {
+                // 如果文件夹不存在，则创建该文件夹
+                Directory.CreateDirectory(extractFolder);
+            }
+
+            // 提取 .zip 文件到指定文件夹
+            ZipFile.ExtractToDirectory(zipFile, extractFolder);
+            Console.WriteLine("文件解压完成");
+        }
+
+    }
+}
---
+++ FileCompDecompExercise/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace FileCompDecompExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("追逐时光者 Hello, World!");
+
+            string sourceFolder = @".\source"; //要压缩的文件夹路径
+            string extractFolder = @".\target\extracted"; //解压后的文件夹路径
+
+            //压缩后的zip文件路径
+            string zipFile = @".\target\compressed.zip";
+
+            //创建一个新的 .zip 文件并压缩文件夹内容
+            FileCompressionHelper.CreateZipFile(sourceFolder, zipFile);
+
+            //提取 .zip 文件到一个新文件夹
+            FileCompressionHelper.ExtractZipFile(zipFile, extractFolder);
+
+            Console.WriteLine("操作完成");
+        }
+    }
+}
