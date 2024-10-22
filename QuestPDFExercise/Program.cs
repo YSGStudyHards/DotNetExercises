97751a7088e5dc8fb8224a6128b905532b34d890
Edwin <1070342164@qq.com>
2024-10-22 21:59:28 +0800

    Update QuestPDFExercise

--- QuestPDFExercise/Program.cs
+++ QuestPDFExercise/Program.cs
@@ -8,17 +8,17 @@ namespace QuestPDFExercise
     {
         static void Main(string[] args)
         {
-            // 1、请确保您有资格使用社区许可证，不设置的话会报异常。
+            //1、请确保您有资格使用社区许可证，不设置的话会报异常。
             Settings.License = LicenseType.Community;
 
-            // 2、禁用QuestPDF库中文本字符可用性的检查
+            //2、禁用QuestPDF库中文本字符可用性的检查
             Settings.CheckIfAllTextGlyphsAreAvailable = false;
 
-            // 3、PDF Document 创建
+            //3、PDF Document 创建
             var invoiceSourceData = CreateInvoiceDetails.GetInvoiceDetails();
             var document = new CreateInvoiceDocument(invoiceSourceData);
 
-            // 4、生成 PDF 文件并在默认的查看器中显示
+            //4、生成 PDF 文件并在默认的查看器中显示
             document.GeneratePdfAndShow();
         }
     }
