e1d5ce63b1432679484da90613820d4d3df49316
Edwin <1070342164@qq.com>
2025-08-09 14:37:26 +0800

    update

--- SundialExercises/Program.cs
+++ SundialExercises/Program.cs
@@ -20,13 +20,13 @@ namespace SundialExercises
                 //options.AddJob<CustomJob>(Triggers.PeriodSeconds(5));
 
                 // 添加自定义任务（每10秒执行）
-                //options.AddJob<CustomJob>(Triggers.PeriodSeconds(10));
+                options.AddJob<CustomJob>(Triggers.PeriodSeconds(10));
 
                 //.NET Cron 表达式解析库 TimeCrontab：https://gitee.com/dotnetchina/TimeCrontab
-                var crontab = Crontab.DailyAt(3); // 每天第 3 小时正（点）
+                //var crontab = Crontab.DailyAt(3); // 每天第 3 小时正（点）
                 //var crontab = Crontab.WeeklyAt("WED");  // SUN（星期天），MON，TUE，WED，THU，FRI，SAT
                 //var crontab = Crontab.YearlyAt(3); // 每年第 3，5，6 月 1 日零点正
-                options.AddJob<CustomJob>(Triggers.Cron(crontab.ToString()));
+                //options.AddJob<CustomJob>(Triggers.Cron(crontab.ToString()));
             });
 
             var app = builder.Build();
